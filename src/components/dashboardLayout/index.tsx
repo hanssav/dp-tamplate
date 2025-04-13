5f65a0c97f723dfcd21a078af89a3b0a6c3ea21e
hanssav <handihandi40@gmail.com>
2025-04-13 23:04:52 +0700

    add function for autor scroll to up when change page
    Signed-off-by: hanssav <handihandi40@gmail.com>

--- src/components/dashboardLayout/index.tsx
+++ src/components/dashboardLayout/index.tsx
@@ -1,6 +1,8 @@
 import NavbarMenu from '@components/Navbar';
 import SidebarMenu from '@components/SideBar';
-import { useState, ReactNode } from 'react';
+import { getTitleFromPath } from '@utils/function';
+import { useState, ReactNode, useEffect, useRef } from 'react';
+import { useLocation } from 'react-router';
 
 interface DashboardLayoutProps {
   children: ReactNode;
@@ -10,21 +12,31 @@ const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState(false);
 
-  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
-  const toggleMobileSidebar = () =>
-    setIsMobileSidebarOpen(!isMobileSidebarOpen);
+  const scrollableRef = useRef<HTMLDivElement>(null); // ✅ Tambahkan ref
+  const location = useLocation();
+  const title = getTitleFromPath(location.pathname);
+
+  useEffect(() => {
+    // ✅ Scroll di elemen yang benar
+    scrollableRef.current?.scrollTo({ top: 0, behavior: 'auto' });
+  }, [title]);
 
   return (
-    <div className="flex h-screen font-sans dark:bg-dark-bg-main dark:text-white ">
+    <div className="flex h-screen font-sans dark:bg-dark-bg-main dark:text-white">
       <SidebarMenu
         isSidebarOpen={isSidebarOpen}
         isMobileSidebarOpen={isMobileSidebarOpen}
-        toggleMobileSidebar={toggleMobileSidebar}
+        toggleMobileSidebar={() => setIsMobileSidebarOpen(!isMobileSidebarOpen)}
       />
-      <div className="flex flex-1 flex-col overflow-y-scroll">
+      <div
+        ref={scrollableRef} // ✅ pasang ref di elemen scrollable
+        className="flex flex-1 flex-col overflow-y-scroll"
+      >
         <NavbarMenu
-          toggleMobileSidebar={toggleMobileSidebar}
-          toggleSidebar={toggleSidebar}
+          toggleMobileSidebar={() =>
+            setIsMobileSidebarOpen(!isMobileSidebarOpen)
+          }
+          toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)}
         />
         <main className="container my-5">{children}</main>
       </div>
