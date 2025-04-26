eb67e6aa1ab7cf7288a48825558873e82f1bd898
hanssav <handihandi40@gmail.com>
2025-04-26 16:34:46 +0700

    delete old theme version
    Signed-off-by: hanssav <handihandi40@gmail.com>

--- src/components/Button/GetButtonStyle.ts
+++ src/components/Button/GetButtonStyle.ts
@@ -23,25 +23,3 @@ export const customButtonTheme = createTheme({
     lg: 'rounded-[0.5rem]',
   },
 });
-
-export const theme = {
-  colors: {
-    primary:
-      'bg-primary text-white focus:outline-none focus:ring-0 enabled:hover:bg-primary/90 dark:bg-primary dark:focus:ring-0 dark:enabled:hover:bg-primary/90',
-    secondary:
-      'border border-primary bg-transparent text-primary focus:ring-0 enabled:hover:bg-soft-blue dark:bg-transparent dark:enabled:hover:bg-dark-hover',
-    light:
-      'bg-transparent text-gray-900 focus:ring-0 focus:ring-transparent enabled:hover:bg-soft-blue dark:bg-transparent dark:text-white dark:enabled:hover:bg-gray-700',
-  } as { [key: string]: string },
-  sizes: {
-    xs: 'px-0 py-1 text-sm',
-    sm: 'px-0.5 py-1 text-sm',
-    md: 'px-4 py-1 text-sm',
-    lg: 'px-5 py-1.5 text-base',
-    icon: 'w-10 h-10 flex items-center justify-center',
-  },
-  // https://tailwindcss.com/docs/border-radius#creating-pill-buttons
-  rounded: {
-    lg: 'rounded-[0.5rem]',
-  },
-};
